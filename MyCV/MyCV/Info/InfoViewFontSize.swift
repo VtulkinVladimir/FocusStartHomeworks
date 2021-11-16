bad1ea93aae2f28576c563b8449dac252ac679e5
VtulkinVladimir <91710529+VtulkinVladimir@users.noreply.github.com>
2021-11-17 01:43:58 +0700

    исправление "магических чисел"
    добавил энумы, описывающие хранимые свойства

---
+++ MyCV/MyCV/Info/InfoViewFontSize.swift
@@ -0,0 +1,14 @@
+//
+//  FontSize.swift
+//  MyCV
+//
+//  Created by Владимир Втулкин on 17.11.2021.
+//
+import UIKit
+
+enum InfoViewFontSize
+{
+	static let fullName: CGFloat = 40
+	static let age: CGFloat = 35
+	static let city: CGFloat = 25
+}
