da22a4758cd849833a5752ca61b583c1dc7dea60
VtulkinVladimir <91710529+VtulkinVladimir@users.noreply.github.com>
2021-12-01 00:56:01 +0700

    Дублировал проект с прошлой домашки
    три экрана. первый с коллекшн вью, второй с детальными данными и третий, открывающийся модально

---
+++ CollectionApp with architecture/CollectionApp/FirstScreen(MVC)/CarCollectionCell.swift
@@ -0,0 +1,97 @@
+//
+//  CarCollectionCell.swift
+//  CollectionApp
+//
+//  Created by Владимир Втулкин on 19.11.2021.
+//
+
+import UIKit
+import SnapKit
+
+final class CarCollectionCell: UICollectionViewCell
+{
+	static let identifier = "CarCell"
+	
+	private let imageView = UIImageView()
+	private let manufacturer = UILabel()
+	private let model = UILabel()
+	private let yearOfIssue = UILabel()
+	private let fontSize = TextSize.carCollectionCellTextSize
+
+	
+	var car: MainScreenCarModel? {
+		didSet {
+			self.setContent()
+		}
+	}
+	
+	override init(frame: CGRect) {
+		super.init(frame: frame)
+		self.configureView()
+	}
+	
+	
+	required init?(coder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+	
+	override var isHighlighted: Bool {
+		didSet {
+			UIView.animate(withDuration: 0.25) {
+				self.alpha = self.isHighlighted ? 0.5 : 1.0
+			}
+		}
+	}
+	
+	private func setContent() {
+		guard let car = self.car else { return }
+		self.imageView.image = car.image
+		self.imageView.contentMode = .scaleAspectFit
+		self.imageView.translatesAutoresizingMaskIntoConstraints = false
+
+		self.manufacturer.text = car.manufacturer
+		self.manufacturer.font = .boldSystemFont(ofSize: self.fontSize)
+		self.manufacturer.shadowColor = .lightText
+		self.manufacturer.translatesAutoresizingMaskIntoConstraints = false
+
+		self.model.text = car.model
+		self.model.font = .boldSystemFont(ofSize: self.fontSize)
+		self.model.shadowColor = .lightText
+		self.model.translatesAutoresizingMaskIntoConstraints = false
+
+		self.yearOfIssue.text = String(car.yearOfIssue)
+		self.yearOfIssue.font = .boldSystemFont(ofSize: self.fontSize)
+		self.yearOfIssue.shadowColor = .lightText
+		self.yearOfIssue.translatesAutoresizingMaskIntoConstraints = false
+	}
+
+	private func configureView() {
+		self.backgroundColor = .systemFill
+		
+		self.addSubview(self.imageView)
+		self.imageView.snp.makeConstraints { maker in
+			maker.height.equalToSuperview().offset(-10)
+			maker.width.equalTo(self.imageView.snp.height)
+			maker.leading.equalToSuperview().offset(5)
+			maker.centerY.equalToSuperview()
+		}
+
+		self.addSubview(self.manufacturer)
+		self.manufacturer.snp.makeConstraints { maker in
+			maker.leading.equalTo(self.imageView.snp.trailing).offset(10)
+			maker.centerY.equalTo(self.imageView.snp.centerY).offset(-50)
+		}
+
+		self.addSubview(self.model)
+		self.model.snp.makeConstraints { maker in
+			maker.leading.equalTo(self.manufacturer.snp.trailing).offset(10)
+			maker.centerY.equalTo(self.manufacturer.snp.centerY)
+		}
+
+		self.addSubview(self.yearOfIssue)
+		self.yearOfIssue.snp.makeConstraints { maker in
+			maker.top.equalTo(self.manufacturer.snp.bottom).offset(20)
+			maker.leading.equalTo(self.imageView.snp.trailing).offset(10)
+		}
+	}
+}
---
+++ CollectionApp with architecture/CollectionApp/TextSize.swift
@@ -0,0 +1,15 @@
+//
+//  TextSize.swift
+//  CollectionApp
+//
+//  Created by Владимир Втулкин on 21.11.2021.
+//
+import UIKit
+
+struct TextSize
+{
+	static let carCollectionCellTextSize: CGFloat = 20
+	static let priceDetailTextSize: CGFloat = 35
+	static let carDetailTextSize: CGFloat = 30
+	static let carDetailHorizontalTextSize: CGFloat = 45
+}
